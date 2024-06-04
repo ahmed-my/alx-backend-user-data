60361780606280a571b4d7bd38efaa7f062c5c67
my-ahmed <yahaya.mymohammed@gmail.com>
2024-06-04 20:19:38 +0000

    Basic authentication

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -21,11 +21,37 @@ def not_found(error) -> str:
     return jsonify({"error": "Not found"}), 404
 
 
-@app.errorhandler(401)
+@app.errorhandler(403)
 def unauthorized(error) -> str:
     """Unauthorized handler task1.
     """
-    return jsonify({"error": "Unauthorized"}), 401
+    return jsonify({"error": "Unauthorized"}), 403
+
+
+@app.errorhandler(401)
+def forbidden(error) -> str:
+    """Forbidden handler task2.
+    """
+    return jsonify({"error": "Forbidden"}), 401
+
+
+@app.before_request
+def authenticate_user():
+    """Authenticates a user before processing a request.
+    """
+    if auth:
+        excluded_paths = [
+            '/api/v1/status/',
+            '/api/v1/unauthorized/',
+            '/api/v1/forbidden/',
+        ]
+        if auth.require_auth(request.path, excluded_paths):
+            auth_header = auth.authorization_header(request)
+            user = auth.current_user(request)
+            if auth_header is None:
+                abort(401)
+            if user is None:
+                abort(403)
 
 
 if __name__ == "__main__":
