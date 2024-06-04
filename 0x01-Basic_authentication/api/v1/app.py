ecdc605139a66d29d4c2b259c44ea6d8fa21c01e
my-ahmed <yahaya.mymohammed@gmail.com>
2024-06-04 20:55:43 +0000

    Basic authentication

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -1,38 +1,46 @@
 #!/usr/bin/env python3
+"""Route module for the API.
 """
-Route module for the API
-"""
+import os
 from os import getenv
-from api.v1.views import app_views
 from flask import Flask, jsonify, abort, request
 from flask_cors import (CORS, cross_origin)
-import os
+
+from api.v1.views import app_views
+from api.v1.auth.auth import Auth
+from api.v1.auth.basic_auth import BasicAuth
 
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
 CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
+auth = None
+auth_type = getenv('AUTH_TYPE', 'auth')
+if auth_type == 'auth':
+    auth = Auth()
+if auth_type == 'basic_auth':
+    auth = BasicAuth()
 
 
 @app.errorhandler(404)
 def not_found(error) -> str:
-    """ Not found handler task 0
+    """Not found handler.
     """
     return jsonify({"error": "Not found"}), 404
 
 
-@app.errorhandler(403)
-def unauthorized(error) -> str:
-    """Unauthorized handler task1.
-    """
-    return jsonify({"error": "Unauthorized"}), 403
-
-
 @app.errorhandler(401)
-def forbidden(error) -> str:
-    """Forbidden handler task2.
+def unauthorized(error) -> str:
+    """Unauthorized handler.
     """
-    return jsonify({"error": "Forbidden"}), 401
+    return jsonify({"error": "Unauthorized"}), 401
+
+
+@app.errorhandler(403)
+def forbidden(error) -> str:
+    """Forbidden handler.
+    """
+    return jsonify({"error": "Forbidden"}), 403
 
 
 @app.before_request
